d6a7a1c54fc2099c42a9b7683b6ca64816a5a6cd
ormr <seraf.gavrilov@gmail.com>
2022-09-22 17:47:36 +0300

    added pages

---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Main = () => (
+  <div>
+    <h1>Main</h1>
+  </div>
+);
---
+++ client/src/pages/Profile/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Profile = () => (
+  <div>
+    <div>Profile</div>
+  </div>
+);
