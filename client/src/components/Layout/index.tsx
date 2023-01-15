28362bf70a96eabd2f912eaf583e6079dc9a9d00
ormr <seraf.gavrilov@gmail.com>
2023-01-15 18:42:14 +0300

    improved post modal

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, ReactElement, FC, ReactNode } from 'react';
+import React, { useState, useEffect, useMemo, FC, ReactNode, useRef } from 'react';
 import { useAppDispatch } from '../../store/hooks';
 import { requestUserData } from '../../features/user/actions';
 import Header from '../Header';
@@ -6,6 +6,9 @@ import PlusIcon from '../../assets/PlusIcon';
 import ImageIcon from '../../assets/ImageIcon';
 import VideoIcon from '../../assets/VideoIcon';
 import PaperclipIcon from '../../assets/Paperclip';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 
 interface LayoutProps {
   children: ReactNode;
@@ -41,25 +44,107 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   );
 };
 
-const CreatePost = () => (
-  <div className="absolute top-0 bottom-0 right-0 left-0 bg-neutral-700 bg-opacity-50">
-    <div className="w-full h-72 fixed bottom-0 bg-white py-6 px-3">
-      <h3 className="mb-3 font-bold">Create post</h3>
-      <textarea
-        className="h-30 py-2.5 px-2 w-full shadow-none block border-2 border-neutral-300 focus:ring-blue-500 focus:border-blue-500 rounded-sm resize-none"
-        placeholder="Start typing here..."
-      />
-      <div className="w-full flex items-center gap-3.5 mt-2 mb-4">
-        <p className="text-xs text-gray-400">Add:</p>
-        <button><ImageIcon /></button>
-        <button><VideoIcon /></button>
-        <button><PaperclipIcon /></button>
-        <div className="flex-1 flex justify-end">div</div>
+const CreatePost = () => {
+  const maxValue = 250;
+  const rawText = useRef('');
+  const [plainText, setPlainText] = useState('');
+
+  const handleChange = ({ target }: any) => {
+    // if (target.value.length <= maxValue) {
+      rawText.current = target.value;
+      setPlainText(target.value);
+    // } 
+  }
+
+  return (
+    <div className="absolute top-0 bottom-0 right-0 left-0 bg-neutral-700 bg-opacity-50">
+      <div className="w-full h-72 fixed bottom-0 bg-white py-6 px-3">
+        <h3 className="mb-3 font-bold">Create post</h3>
+        <Textarea value={rawText.current} onChange={handleChange}/>
+        <div className="w-full flex items-center gap-3.5 mt-2 mb-4">
+          <p className="text-xs text-gray-400">Add:</p>
+          <button><ImageIcon /></button>
+          <button><VideoIcon /></button>
+          <button><PaperclipIcon /></button>
+          <Progressbar value={plainText.length} maxValue={maxValue}/>
+        </div>
+        <button className="w-full p-4 bg-black text-white border-1">Post</button>
       </div>
-      <button className="w-full p-4 bg-black text-white border-1">Post</button>
     </div>
-  </div>
+  );
+};
+
+
+interface TextareaProps {
+  value: string;
+  onChange: ((event: ContentEditableEvent) => void);
+}
+
+const Textarea: FC<TextareaProps> = ({ value, onChange }) => (
+  <ContentEditable
+    className="h-30 py-2.5 px-2 w-full shadow-none block border-2 border-neutral-300 focus:ring-blue-500 focus:border-blue-500 rounded-sm resize-none overflow-hidden"
+    html={value}
+    onChange={onChange}
+  />
 );
+
+interface ProgressbarProps {
+  value: number;
+  maxValue: number;
+}
+
+
+const Progressbar: FC<ProgressbarProps> = ({ value, maxValue }) => {
+  const getTextFullness = () => {
+    if (value >= (maxValue - 20) && value < maxValue) {
+      return '#71767B';
+    }
+
+    if (value >= maxValue) {
+      return '#F4212E';
+    }
+
+    return '#1D9BF9';
+  };
+
+  const getPathColor = () => {
+    if (value >= (maxValue - 20) && value < maxValue) {
+      return '#FFD400';
+    }
+
+    if (value >= maxValue && value <= maxValue + 10) {
+      return '#F4212E';
+    }
+
+    if (value > maxValue + 10) {
+      return 'transparent';
+    }
+  }
+
+  const getTrailColor = () => {
+    if (value > maxValue + 10) {
+      return 'transparent';
+    }
+  }
+
+  return (
+    <div className="flex-1 flex justify-end">
+      <div className="w-7 h-7">
+        <CircularProgressbar
+          value={value}
+          maxValue={maxValue}
+          text={(value >= (maxValue - 20)) ? `${maxValue - value}` : ''}
+          styles={buildStyles({
+            textSize: '2.5rem',
+            pathColor: getPathColor(),
+            textColor: getTextFullness(),
+            trailColor: getTrailColor(),
+          })}
+        />
+      </div>
+    </div>
+  )
+};
 
 interface AddPostButton {
   onClick: VoidFunction;
