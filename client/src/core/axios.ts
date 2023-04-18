5033fe1c1ab2e0dfeb3bd1a2ec49adc03f89ce66
ormr <seraf.gavrilov@gmail.com>
2023-04-18 14:46:53 +0300

    added dot env server url path

--- client/src/core/axios.ts
+++ client/src/core/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:3001/';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const Axios = axios.create({
   baseURL: BASE_URL,
