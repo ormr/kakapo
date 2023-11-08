01973e7d2ad3fcda996e62d753b192d719ed580f
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2023-11-08 09:07:58 +0400

    added api prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+  app.setGlobalPrefix('/api');
   app.enableCors({
     origin: config.get<string>('API_ORIGIN_URL'),
     credentials: true,
